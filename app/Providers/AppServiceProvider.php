bcf099bc421120165eb7df003c0bde1585dded8c
Tiemsky <tiafranck31@yahoo.fr>
2026-03-25 15:58:24 +0000

    Fix: Forcig Https On Production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+      if (app()->environment('production')) {
+           URL::forceScheme('https');
+      }
         Vite::prefetch(concurrency: 3);
          JsonResource::withoutWrapping();
     }
