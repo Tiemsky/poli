0bb26c8dd25cdf67179d94a1ba74a409e4ae9041
Tiemsky <tiafranck31@yahoo.fr>
2026-03-25 22:15:52 +0000

    Fix: Removing UserFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+// use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,15 +22,15 @@ class DatabaseSeeder extends Seeder
         CommuneSeeder::class,
     ]);
 
-  if (User::whereIn('phone', ['0767189538', '0202020202'])->count() === 0) {
-    User::factory()->withPhone('0767189538')
-        ->withRole('user')
-        ->create(['first_name' => 'Alice', 'last_name' => 'Dupont']);
+//   if (User::whereIn('phone', ['0767189538', '0202020202'])->count() === 0) {
+//     User::factory()->withPhone('0767189538')
+//         ->withRole('user')
+//         ->create(['first_name' => 'Alice', 'last_name' => 'Dupont']);
 
-    User::factory()->withPhone('0202020202')
-        ->withRole('admin')
-        ->create(['first_name' => 'Bob', 'last_name' => 'Martin']);
-}
+//     User::factory()->withPhone('0202020202')
+//         ->withRole('admin')
+//         ->create(['first_name' => 'Bob', 'last_name' => 'Martin']);
+// }
 
     }
 }
